2a7c17a9933d2381ecf06b92c2c03e9d79970edb
Henry Firth <henrynf12@gmail.com>
2022-09-15 21:51:50 -0500

    Add tests for Union

--- sets_test.go
+++ sets_test.go
@@ -45,7 +45,53 @@ func TestSingletonSet(t *testing.T) {
 	}
 }
 
-// TestUnion
+func TestUnion(t *testing.T) {
+	tests := []struct {
+		Name string
+
+		Set1 Set
+		Set2 Set
+
+		InputValues    []int
+		ExpectedResult bool
+	}{
+		{
+			Name: "union contains both values of two singleton sets",
+
+			Set1: SingletonSet(1),
+			Set2: SingletonSet(2),
+
+			InputValues:    []int{1, 2},
+			ExpectedResult: true,
+		},
+		{
+			Name: "union does not contain value that is in neither of two singleton sets",
+
+			Set1: SingletonSet(1),
+			Set2: SingletonSet(2),
+
+			InputValues:    []int{3},
+			ExpectedResult: false,
+		},
+	}
+
+	for _, entry := range tests {
+		t.Run(entry.Name, func(t *testing.T) {
+			set := Union(entry.Set1, entry.Set2)
+
+			for _, v := range entry.InputValues {
+				result := set(v)
+				if result != entry.ExpectedResult {
+					t.Errorf("Expected set(%+v) to be %+v, but got %+v",
+						v,
+						entry.ExpectedResult,
+						result,
+					)
+				}
+			}
+		})
+	}
+}
 
 // TestIntersect
 
